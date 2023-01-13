0796d4c78f7d66cf17e46e61ea26e932865ddaa2
elabt95 <elamine.bentalha95@gmail.com>
2023-01-13 20:08:31 +0100

    Create + Factory + Display of Category Table

---
+++ resources/views/Category/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <h3 class="text-primary text-center mt-5 mb-5">Category List </h3>
+    <div class="col-2"></div>
+    <div class="col-8 align-center">
+        <div class="col-4 mb-3">
+
+        </div>
+        <table class="table ">
+            <tr class="bg-primary text-white">
+                <th>ID</th>
+                <th>Name</th>
+                <th>Parent</th>
+                <th>Action</th>
+            </tr>
+            @foreach($categories as $categorie)
+            <tr>
+                <td>{{$categorie->id}}</td>
+                <td>{{$categorie->name}}</td>
+                <td>
+                    @if (empty(!$categorie->category_parent))
+                    {{$categorie->category_parent->name}}
+                    @else
+                    -
+                    @endif
+                </td>
+            </tr>
+            @endforeach
+
+        </table>
+    </div>
+</div>
+
+
+@endsection
